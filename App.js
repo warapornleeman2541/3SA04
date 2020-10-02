76a8e4ba62029ec02f0212991b1359d777c5d015
warapornleeman2541 <s6035512008@phuket.psu.ac.th>
2020-10-02 21:08:42 +0700

    3SA04

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import ZipCodeScreen from "./components/ZipCodeScreen";
-import WeatherScreen from "./components/WeatherScreen";
+//import liraries
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Weather from './components/Weather'
+import WeatherScreen from './components/WeatherScreen'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import ZipCodeScreen from './components/ZipCodeScreen';
 
 const Stack = createStackNavigator();
 
-export default function App() {
+// create a component
+const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={ZipCodeScreen} />
         <Stack.Screen name="Weather" component={WeatherScreen} />
-
-      </Stack.Navigator>
-    </NavigationContainer>
+      </Stack.Navigator> 
+    </NavigationContainer>       
   );
-}
+};
+
+// define your styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+  },
+});
+
+//make this component available to the app
+export default App;
